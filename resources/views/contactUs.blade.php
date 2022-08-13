3561b9bef5cb0ee2862b4bec9622a78a9960a4f4
Mohamed <mido.b333@gmail.com>
2022-08-13 21:39:03 +0400

    hide contact form

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -38,72 +38,72 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-12 col-sm-12">
-                    <p class="text-center">Want to get in touch? Fill out the form below to send me a message and I will get back to you as soon as possible!</p>
-                    <!-- Contact Form - Enter your email address on line 19 of the mail/contact_me.php file to make this form work. -->
-                    <!-- WARNING: Some web hosts do not allow emails to be sent through forms to common mail hosts like Gmail or Yahoo. It's recommended that you use a private domain email address! -->
-                    <!-- To use the contact form, your site must be on a live web host with PHP! The form will not work locally! -->
-                    <div id="Show Messages">
-                        @if(session('success'))
-                            <div class="alert alert-success text-center" role="alert">
-                                {{ session('success') }}
-                            </div>
-                        @endif
-                    </div>
-                    <form name="sentMessage" id="contactForm" action="{{ route('sendMail') }}" method="post">
-                        @csrf
-                        <div class="control-group">
-                            <div class="form-group floating-label-form-group controls">
-                                <label>Name</label>
-                                <input name="name" value="{{ old('name') }}" type="text" class="form-control" placeholder="Name" id="name">
-                                @if ($errors->has('name'))
-                                    <div class="text-danger" role="alert">{{ $errors->first('name') }}</div>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <div class="form-group floating-label-form-group controls">
-                                <label>Email Address</label>
-                                <input name="email" value="{{ old('email') }}" type="email" class="form-control" placeholder="Email Address" id="email">
-                                @if ($errors->has('email'))
-                                    <div class="text-danger" role="alert">{{ $errors->first('email') }}</div>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Phone Number</label>
-                                <input name="phone" value="{{ old('phone') }}" type="tel" class="form-control" placeholder="Phone Number" id="phone">
-                                @if ($errors->has('phone'))
-                                    <div class="text-danger" role="alert">{{ $errors->first('phone') }}</div>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <div class="form-group floating-label-form-group controls">
-                                <label>Message</label>
-                                <textarea name="body" value="{{ old('body') }}" rows="5" class="form-control" placeholder="Message" id="body"></textarea>
-                                @if ($errors->has('body'))
-                                    <div class="text-danger" role="alert">{{ $errors->first('body') }}</div>
-                                @endif
-                            </div>
-                        </div>
-                        <br>
-                        @if ($errors->has('g-recaptcha-response'))
-                            <span class="help-block" style="color: red">
-                                <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
-                            </span>
-                        @endif
-                        <div class="form-group">
-                            {!! NoCaptcha::renderJs() !!}
-                            {!! NoCaptcha::display() !!}
-                        </div>
-                        <br>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary" id="sendMessageButton">Send</button>
-                        </div>
-                    </form>
-                </div>
+{{--                <div class="col-lg-6 col-md-12 col-sm-12">--}}
+{{--                    <p class="text-center">Want to get in touch? Fill out the form below to send me a message and I will get back to you as soon as possible!</p>--}}
+{{--                    <!-- Contact Form - Enter your email address on line 19 of the mail/contact_me.php file to make this form work. -->--}}
+{{--                    <!-- WARNING: Some web hosts do not allow emails to be sent through forms to common mail hosts like Gmail or Yahoo. It's recommended that you use a private domain email address! -->--}}
+{{--                    <!-- To use the contact form, your site must be on a live web host with PHP! The form will not work locally! -->--}}
+{{--                    <div id="Show Messages">--}}
+{{--                        @if(session('success'))--}}
+{{--                            <div class="alert alert-success text-center" role="alert">--}}
+{{--                                {{ session('success') }}--}}
+{{--                            </div>--}}
+{{--                        @endif--}}
+{{--                    </div>--}}
+{{--                    <form name="sentMessage" id="contactForm" action="{{ route('sendMail') }}" method="post">--}}
+{{--                        @csrf--}}
+{{--                        <div class="control-group">--}}
+{{--                            <div class="form-group floating-label-form-group controls">--}}
+{{--                                <label>Name</label>--}}
+{{--                                <input name="name" value="{{ old('name') }}" type="text" class="form-control" placeholder="Name" id="name">--}}
+{{--                                @if ($errors->has('name'))--}}
+{{--                                    <div class="text-danger" role="alert">{{ $errors->first('name') }}</div>--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="control-group">--}}
+{{--                            <div class="form-group floating-label-form-group controls">--}}
+{{--                                <label>Email Address</label>--}}
+{{--                                <input name="email" value="{{ old('email') }}" type="email" class="form-control" placeholder="Email Address" id="email">--}}
+{{--                                @if ($errors->has('email'))--}}
+{{--                                    <div class="text-danger" role="alert">{{ $errors->first('email') }}</div>--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="control-group">--}}
+{{--                            <div class="form-group col-xs-12 floating-label-form-group controls">--}}
+{{--                                <label>Phone Number</label>--}}
+{{--                                <input name="phone" value="{{ old('phone') }}" type="tel" class="form-control" placeholder="Phone Number" id="phone">--}}
+{{--                                @if ($errors->has('phone'))--}}
+{{--                                    <div class="text-danger" role="alert">{{ $errors->first('phone') }}</div>--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="control-group">--}}
+{{--                            <div class="form-group floating-label-form-group controls">--}}
+{{--                                <label>Message</label>--}}
+{{--                                <textarea name="body" value="{{ old('body') }}" rows="5" class="form-control" placeholder="Message" id="body"></textarea>--}}
+{{--                                @if ($errors->has('body'))--}}
+{{--                                    <div class="text-danger" role="alert">{{ $errors->first('body') }}</div>--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <br>--}}
+{{--                        @if ($errors->has('g-recaptcha-response'))--}}
+{{--                            <span class="help-block" style="color: red">--}}
+{{--                                <strong>{{ $errors->first('g-recaptcha-response') }}</strong>--}}
+{{--                            </span>--}}
+{{--                        @endif--}}
+{{--                        <div class="form-group">--}}
+{{--                            {!! NoCaptcha::renderJs() !!}--}}
+{{--                            {!! NoCaptcha::display() !!}--}}
+{{--                        </div>--}}
+{{--                        <br>--}}
+{{--                        <div class="form-group">--}}
+{{--                            <button type="submit" class="btn btn-primary" id="sendMessageButton">Send</button>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
             </div>
         </div>
     </div>
